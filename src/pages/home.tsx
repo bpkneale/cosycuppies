d92b4d86b1db836bf9684de5969686017ceaad95
Ben Kneale <bpkneale@gmail.com>
2021-01-16 08:07:55 +0800

    Added carousel to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { connect } from "react-redux";
+import { Carousel } from "react-responsive-carousel"
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "./home.css"
 
 type ComponentProps = {
 }
@@ -14,8 +17,23 @@ type Props = ComponentProps & StateProps & DispatchProps;
 
 class HomePage extends React.Component<Props> {
     render() {
-        return <div>
-
+        return <div className="home">
+            <div className="carousel-container">
+                <Carousel className="carousel" autoPlay={true} infiniteLoop={true} interval={5000}>
+                    <div>
+                        <img src="/assets/carousel/1.jpg" />
+                        <p>Bespoke hand-made cupcakes for any occasion</p>
+                    </div>
+                    <div>
+                        <img src="/assets/carousel/2.jpg" />
+                        <p>Customisable flavours, swirls, and more</p>
+                    </div>
+                    <div>
+                        <img src="/assets/carousel/3.jpg" />
+                        <p>Not just cupcakes!</p>
+                    </div>
+                </Carousel>
+            </div>
         </div>
     }
 }
