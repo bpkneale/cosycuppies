63a984c3acf99a7006277ad6667fb4b101a2166e
Ben Kneale <bpkneale@gmail.com>
2021-01-17 09:32:01 +0800

    Added cupcake flavours. Added add to cart

--- src/components/cupcakeconfiguration.tsx
+++ src/components/cupcakeconfiguration.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from "react-redux";
 import { FrostingFlavour, FrostingFlavours } from "../data/frostingflavours"
+import { CupcakeFlavour, CupcakeFlavours } from "../data/cupcakeflavours"
 import "./cupcakeconfiguration.css"
 
 type ComponentProps = {
@@ -16,15 +17,9 @@ type Props = ComponentProps & StateProps & DispatchProps;
 
 type State = {
     amount: number;
-    flavour: FrostingFlavour;
+    cupcakeFlavour: CupcakeFlavour | undefined;
+    frostingFlavour: FrostingFlavour | undefined;
     box: boolean;
-}
-
-const FrostingFlavourDisp = (flavour: FrostingFlavour, curr: FrostingFlavour, onSelect: (f: FrostingFlavour) => void) => {
-    return <div key={flavour.flavour}>
-        <input id={`flavour-inp-${flavour.flavour}`} type="radio" checked={curr === flavour} onChange={() => onSelect(flavour)} />
-        <label htmlFor={`flavour-inp-${flavour.flavour}`}>{flavour.flavour}</label>
-    </div>
 }
 
 class CupcakeConfigurationUnc extends React.Component<Props, State> {
@@ -32,13 +27,18 @@ class CupcakeConfigurationUnc extends React.Component<Props, State> {
         super(props);
         this.state = {
             amount: 12,
-            flavour: FrostingFlavours[0],
+            cupcakeFlavour: CupcakeFlavours[0],
+            frostingFlavour: FrostingFlavours[0],
             box: false
         }
     }
 
+    onSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        event.preventDefault();
+    }
+
     render() {
-        const { amount, flavour, box } = this.state;
+        const { amount, frostingFlavour, cupcakeFlavour, box } = this.state;
         return <div className="cupcake-conf">
             <form>
                 <p>Cupcake options</p>
@@ -51,14 +51,25 @@ class CupcakeConfigurationUnc extends React.Component<Props, State> {
                     </select>
                     <label htmlFor="amount">Cupcake quantity</label>
                 </div>
+                <p>Cupcake flavour</p>
+                <div className="flavours">
+                    <select id="flavour" value={cupcakeFlavour?.flavour} onChange={a => this.setState({cupcakeFlavour: CupcakeFlavours.find(f => f.id === a.target.value)})}>
+                        {CupcakeFlavours.map(f => <option value={f.id}>{f.flavour}</option>)}
+                    </select>
+                </div>
                 <p>Frosting flavour</p>
-                <div className="frosting-flavours">
-                    {FrostingFlavours.map(f => FrostingFlavourDisp(f, flavour, f => this.setState({flavour: f})))}
+                <div className="flavours">
+                    <select id="flavour" value={frostingFlavour?.flavour} onChange={a => this.setState({frostingFlavour: FrostingFlavours.find(f => f.id === a.target.value)})}>
+                        {FrostingFlavours.map(f => <option value={f.id}>{f.flavour}</option>)}
+                    </select>
                 </div>
                 <p>Other</p>
                 <div className="box">
                     <input id="box" type="checkbox" onChange={() => this.setState({box: !box})} />
-                    <label htmlFor="box">Cupcake box</label>
+                    <label htmlFor="box">Cupcake box(es)</label>
+                </div>
+                <div>
+                    <button type="submit" onClick={this.onSubmit.bind(this)}>Add to cart</button>
                 </div>
             </form>
         </div>
---
+++ src/data/cupcakeflavours.ts
@@ -0,0 +1,20 @@
+
+export type CupcakeFlavour = {
+    flavour: string;
+    id: string;
+}
+
+export const CupcakeFlavours: CupcakeFlavour[] = [
+    {
+        flavour: "Vanilla",
+        id: "vanilla"
+    },
+    {
+        flavour: "Chocolate",
+        id: "chocolate"
+    },
+    {
+        flavour: "Red velvet",
+        id: "red-velvet"
+    }
+]
