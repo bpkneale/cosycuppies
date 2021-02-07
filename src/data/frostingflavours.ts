64c5f3f704f1fa9c5447b00578d12685f39fa931
Ben Kneale <bpkneale@gmail.com>
2021-02-07 15:48:34 +0800

    Added frosting flavours

--- src/data/frostingflavours.ts
+++ src/data/frostingflavours.ts
@@ -6,11 +6,36 @@ export type FrostingFlavour = {
 
 export const FrostingFlavours: FrostingFlavour[] = [
     {
+        flavour: "Almond",
+        id: "almond"
+    },
+    {
+        flavour: "Banana",
+        id: "banana"
+    },
+    {
+        flavour: "Bubble gum",
+        id: "bubble-gum"
+    },
+    {
         flavour: "Cherry",
         id: "cherry"
     },
     {
         flavour: "Coconut",
         id: "coconut"
+    },
+    {
+        flavour: "Mango",
+        id: "mango"
+    },
+    {
+        flavour: "Passionfruit",
+        id: "passionfruit"
+    },
+    {
+        flavour: "Vanilla",
+        id: "vanilla"
     }
 ]
+
